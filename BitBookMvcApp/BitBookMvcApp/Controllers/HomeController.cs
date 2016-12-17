eda47d58617329c39b1bebd37b5576f4315a0fac
takmilul <tonmoy4222@gmail.com>
2016-12-17 23:15:25 +0600

    last update

--- BitBookMvcApp/BitBookMvcApp/Controllers/HomeController.cs
+++ BitBookMvcApp/BitBookMvcApp/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,13 +13,15 @@ namespace BitBookMvcApp.Controllers
 {
     public class HomeController : Controller
     {
+        BitBookMvcAppDbContext db = new BitBookMvcAppDbContext();
+
         [Authorize]
         public ActionResult Index()
         {
             return View();
         }
 
-        
+
         public ActionResult Logout()
         {
             FormsAuthentication.SignOut();
@@ -35,12 +38,11 @@ namespace BitBookMvcApp.Controllers
         [HttpPost]
         public ActionResult ChangePassword(ChangePassword changePassword)
         {
-            BitBookMvcAppDbContext db = new BitBookMvcAppDbContext();
             if (ModelState.IsValid)
             {
-                int userId = (int) Session["UserId"];
+                int userId = (int)Session["UserId"];
                 Models.User user = db.Users.Find(userId);
-                if (user.Password==changePassword.OldPassword)
+                if (user.Password == changePassword.OldPassword)
                 {
                     user.Password = changePassword.NewPassword;
                     db.Entry(user).State = EntityState.Modified;
@@ -49,6 +51,31 @@ namespace BitBookMvcApp.Controllers
                 }
             }
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Post(Post post)
+        {
+            post.Date = DateTime.Now;
+            post.IsPic = false;
+            post.IsText = true;
+            post.UserId = Session["UserId"] as int?;
+
+            db.Posts.Add(post);
+
+            SqlConnection connection = new SqlConnection();
+            string query = "select distinct p.Id, p.Post, p.PicUrl,p.IsPic IsPic, p.IsText IsText, pr.ProPicId, pr.FullName,p.UserIdfrom Post pleft join FriendList f on p.UserId=f.UserIdleft join Profile pr on p.UserId=pr.UserIdwhere f.UserId=1 or f.FriendId=1";
+            List<Post> posts = db.Posts.OrderByDescending(m => m.Id).ToList();
+            List<Comment> comments = db.Comments.OrderByDescending(m => m.Id).ToList();
+            List<Like> likes = db.Likes.OrderByDescending(m => m.Id).ToList();
+            List<Profile> profiles = db.Profiles.OrderByDescending(m => m.Id).ToList();
+
+            return PartialView("_Post");
+        }
+
+        public ActionResult PostResponse()
+        {
+            return null;
         }
     }
 }
---
+++ BitBookMvcApp/BitBookMvcApp/Models/ViewModels/Status.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BitBookMvcApp.Models.ViewModels
+{
+    public class Status
+    {
+        public string Name { get; set; }
+        public int? ProPicId { get; set; }
+        public Post Post { get; set; }
+        public List<Like> Likes { get; set; }
+        public List<Comment> Comments { get; set; }
+        }
+}
